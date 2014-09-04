fbe66c4c45df5c03d47f1ab97287bce012feaf88
respag <respag@hotmail.com>
2014-09-04 08:26:53 -0500

    1

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hola Mundo!");
+            Console.ReadKey(true);
         }
     }
 }
